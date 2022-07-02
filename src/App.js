0910eb698bb3c710c2b089e2ff259984ca38b99f
zeallot <kookladesu@gmail.com>
2022-07-02 13:02:55 +0700

    remove improts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import { Chart } from './components/Chart';
-import data from './data.json';
 
 function App() {
   return (
-    <Chart datasets={data} />
+    <Chart />
   );
 }
 
